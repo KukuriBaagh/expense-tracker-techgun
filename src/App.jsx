b75cdd5d40a7a9e7c8b42b5c3d4e4976e6e0fa24
KukuriBaagh <howtoccdemo@gmail.com>
2023-07-05 21:27:45 +0530

    new component created from add expense button

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,42 @@
 import Expenses from "./components/ExpenseItem/Expenses"
-import React from "react"
+import React, { useState } from "react"
 import NewExpense from "./components/ExpenseItem/NewExpenses/NewExpense"
 
+let dummy_expense = [
+  {
+    id: 'e1',
+    title: 'School Fee',
+    amount: 300,
+    date: new Date(2023, 3, 18)
+  },
+  {
+    id: 'e2',
+    title: 'House Rent',
+    amount: 700,
+    date: new Date(2023, 5, 8)
+  },
+  {
+    id: 'e3',
+    title: 'Car Insurence',
+    amount: 200,
+    date: new Date(2023, 4, 28)
+  },
+  {
+    id: 'e4',
+    title: 'Food',
+    amount: 500,
+    date: new Date(2023, 6, 11)
+  },
+]
 
 const App = () => {
 
-  let expenses = [
-    {
-      id: 'e1',
-      title: 'School Fee',
-      amount: 300,
-      date: new Date(2023, 3, 18)
-    },
-    {
-      id: 'e2',
-      title: 'House Rent',
-      amount: 700,
-      date: new Date(2023, 5, 8)
-    },
-    {
-      id: 'e3',
-      title: 'Car Insurence',
-      amount: 200,
-      date: new Date(2023, 4, 28)
-    },
-    {
-      id: 'e4',
-      title: 'Food',
-      amount: 500,
-      date: new Date(2023, 6, 11)
-    },
-  ]
+  const [ expenses, setExpenses ] = useState(dummy_expense)
 
   const addExpenseHandler = (expense) => {
-    console.log(expense)
+    const updatedExpense = [ expense, ...expenses ]
+    setExpenses(updatedExpense)
+    console.log(updatedExpense)
   }
 
   return (
